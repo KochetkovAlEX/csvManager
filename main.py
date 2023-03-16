e142dd37e219cba9cb3b684dc0c3e42129155925
Nazaretyan Agasi <nazaretyanaghasi@gmail.com>
2023-03-16 21:07:20 +0300

    показ файла(вывод)

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
 import file
 import list_func
 import ui
 def main():
+	os.system("cls")
 	ui.show_menu()
+	file.read_file("data.csv")
 	try:
 		choice=ui.choice_menu()
 		print(choice)
-	except Exception as e:
-		print(e)
+		if choice==7:
+			ui.show_data(file.read_file("data.csv"))
+			input()
+			main()
+	except Exception as er:
+		print(er)
+		input()
 		main()
 main()
-
--- ui.py
+++ ui.py
@@ -7,13 +7,21 @@ def show_menu():
 	4. Добавление данных в файл
 	5. Удаление данных из файла
 	6. Добавление элементов в группу
+	7. Вывести файл
 	""")
 
 def choice_menu():
 	try:
 		choice=int(input("Введите номер пункта: "))
-		if 1>choice or choice>6:
+		if 1>choice or choice>7:
 			raise ValueError("Данные ложны")
 		else:return choice
-	except Exception as e:
-		raise e
+	except:
+		raise ValueError("Данные ложны")
+
+
+def show_data(input_str):
+	for i in input_str.split("\n"):
+		for j in i.split(";"):
+			print(j,end="\t")
+		print()
