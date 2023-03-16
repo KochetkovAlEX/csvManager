e1d0a5ef7e62c010955ec05a5dc6c606c3645fc8
Nazaretyan Agasi <nazaretyanaghasi@gmail.com>
2023-03-16 20:29:23 +0300

    Исключения

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import list_func
 import ui
 def main():
 	ui.show_menu()
+	try:
+		choice=ui.choice_menu()
+		print(choice)
+	except Exception as e:
+		print(e)
+		main()
 main()
+
--- ui.py
+++ ui.py
@@ -1,2 +1,19 @@
 def show_menu():
-	print("csv file")
+	print("""
+	\tCsv manager
+	1. Создать .csv
+	2. Поиск элементов в файле
+	3. Сортировка данных
+	4. Добавление данных в файл
+	5. Удаление данных из файла
+	6. Добавление элементов в группу
+	""")
+
+def choice_menu():
+	try:
+		choice=int(input("Введите номер пункта: "))
+		if 1>choice or choice>6:
+			raise ValueError("Данные ложны")
+		else:return choice
+	except Exception as e:
+		raise e
