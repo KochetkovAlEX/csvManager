cd34744ad0329fdb1cefb7afc5c1d827fd0dac3c
Alex <leshakochetcov@gmail.com>
2023-03-20 21:42:34 +0300

    Ui update

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import ui
 def main():
 	os.system("cls")
 	ui.show_menu()
-	file.read_file("data.csv")
+	file.read_file("../pythonProject/data.csv")
 	try:
 		choice=ui.choice_menu()
 		print(choice)
 		if choice==7:
-			ui.show_data(file.read_file("data.csv"))
+			ui.show_data(file.read_file("../pythonProject/data.csv"))
 			input()
 			main()
 	except Exception as er:
--- ui.py
+++ ui.py
@@ -23,5 +23,49 @@ def choice_menu():
 def show_data(input_str):
 	for i in input_str.split("\n"):
 		for j in i.split(";"):
-			print(j,end="\t")
+			print(j,end="|")
 		print()
+
+def search_in_file(path,arg,search):
+    with open(path,"r",encoding="utf-8") as file1:
+        file1.readline()
+        text=file1.readlines()
+        print(text)
+        for i in text:
+            i=i.split(";")
+            print(i)
+            if arg=='id':
+                if i[0].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'city':
+                if i[1].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'region':
+                if i[2].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'district':
+                if i[3].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'population':
+                if i[4].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'foundation':
+                if i[5].startswith(search):
+                    print(*i)
+                    break
+            elif arg == 'numb':
+                if i[6].startswith(search):
+                    print(*i)
+                    break
+            else:
+                print("данного аргумента нет")
+                break
+print("id","city","region","district","population","foundation")
+arg=input("Поиск среди чего? ").lower()
+search=input("Что ищем? ").lower()
+search_in_file("text1.txt",arg,search)
