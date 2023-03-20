637caa9e12bc1451dc723481aa8a6fe3226d7842
Alex <leshakochetcov@gmail.com>
2023-03-20 21:58:49 +0300

    малость изменил Ui. Добавил кривой, но рабочий поиск. Не совсем понимаю, где обрабатывать исключения, в будущем будет фикс

--- file.py
+++ file.py
@@ -1,3 +1,8 @@
 def read_file(path):
-		with open(path,"r",encoding="utf-8") as file:
-			return file.read()
+	with open(path,"r",encoding="utf-8") as file:
+		return file.read()
+def text_create(path):
+	with open(path,"r",encoding="utf-8") as file1:
+		file1.readline()
+		text = file1.readlines()
+		return text
--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import ui
 def main():
 	os.system("cls")
 	ui.show_menu()
-	file.read_file("../pythonProject/data.csv")
+	file.read_file("data.csv")
 	try:
 		choice=ui.choice_menu()
 		print(choice)
 		if choice==7:
-			ui.show_data(file.read_file("../pythonProject/data.csv"))
+			ui.show_data(file.read_file("data.csv"))
+			input()
+			main()
+		elif choice==2:
+			print("id", "city", "region", "district", "population", "foundation")
+			arg = input("Поиск среди чего? ").lower()
+			search = input("Что ищем? ")
+			ui.search_in_text(file.text_create("data.csv"),arg,search)
 			input()
 			main()
 	except Exception as er:
